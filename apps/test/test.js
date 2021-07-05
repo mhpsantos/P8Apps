3b92ec41ea8174ad070bee0245f6dd7c186bc2c7
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 12:56:17 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,10 +1,11 @@
 
-function draw() {
+function print() {
+  var testVar = "teste";
   g.clear();
   // draw the current counter value
-  g.drawString("teste", g.getWidth()/2, g.getHeight()/2);
+  g.drawString(testVar, g.getWidth()/2, g.getHeight()/2);
   // optional - this keeps the watch LCD lit up
   g.flip();
 }
 
-draw();
+print();
