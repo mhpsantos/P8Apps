c36e7bf67aabff4542e6b5b7a862ea787ee58530
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 11:06:36 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -3,10 +3,12 @@ require("Font7x11Numeric7Seg").add(Graphics);
 function draw() {
   // Reset the state of the graphics library
   g.reset();
-  // draw the current time (4x size 7 segment)
+  // Clear the area where we want to draw the time
+  g.clearRect(-100,100,100,-100);
+  // draw the current time
+  g.setFontAlign(0,0);
   g.setFont("7x11Numeric7Seg",4);
-  g.setFontAlign(0,0); // align center
-  g.drawString("1234567890", 0, 0, true /*clear background*/);
+  g.drawString("12345678890");
 }
 
 // Clear the screen once, at startup
