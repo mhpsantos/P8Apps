f2c7a0b5f14f1fb5afca454f9e8b4d63e0e9f1f8
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 10:18:26 -0300

    Update big.face.js

--- apps/clock/big.face.js
+++ apps/clock/big.face.js
@@ -8,9 +8,11 @@
         var time = da[4].substr(0, 5).split(":");
         var hours = time[0],
           minutes = time[1];
+        require("FontTeletext10x18Mode7").add(Graphics);
         g.clearRect(0,24,239,239);
         g.setColor(1,1,1);
-        g.setFont("8x16");
+        //g.setfont("Vector",132);
+        g.setFontTeletext10x18Mode7();
         g.drawString(hours,50,24,true);
         g.drawString(minutes,50,132,true);
       }
