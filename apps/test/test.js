79db5f7e40816f692d99f00724acebc92d2d46e1
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 11:00:49 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,19 +1,12 @@
 require("Font7x11Numeric7Seg").add(Graphics);
 
 function draw() {
-   // work out how to display the current time
-  var d = new Date();
-  var h = d.getHours(), m = d.getMinutes();
-  var time = (" "+h).substr(-2) + ":" + ("0"+m).substr(-2);
   // Reset the state of the graphics library
   g.reset();
   // draw the current time (4x size 7 segment)
   g.setFont("7x11Numeric7Seg",4);
-  g.setFontAlign(1,1); // align right bottom
-  g.drawString(time, X, Y, true /*clear background*/);
-  // draw the seconds (2x size 7 segment)
-  g.setFont("7x11Numeric7Seg",2);
-  g.drawString(("0"+d.getSeconds()).substr(-2), X+30, Y, true /*clear background*/);
+  g.setFontAlign(0,0); // align center
+  g.drawString("1234567890", 0, 0, true /*clear background*/);
 }
 
 // Clear the screen once, at startup
