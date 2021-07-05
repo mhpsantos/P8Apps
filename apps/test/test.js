9b02303d0ffb20b40af442a8694ea8589063a805
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 11:14:36 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,10 +1,15 @@
 function draw() {
+  // work out how to display the current time
+  var d = new Date();
+  var h = d.getHours(), m = d.getMinutes();
+  var time = h + ":" + ("0"+m).substr(-2);
+
   // Reset the state of the graphics library
   g.reset();
   // Clear the area where we want to draw the time
-  g.clearRect(50,50,100,120);
+  g.clearRect(50,50,240,240);
   // draw the current time
-  g.drawString("test", 50, 50);
+  g.drawString(time, 50, 50);
 }
 
 // Clear the screen once, at startup
