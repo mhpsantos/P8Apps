8c816166ae66b4dd7bc41ab540b6533795b20f57
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 11:58:57 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,18 +1,12 @@
-function draw() {
-  // work out how to display the current time
-  
-  var time = "teste display";
-
-  // Reset the state of the graphics library
-  g.reset();
-  // draw the current time
-  g.clear();
-  g.setFontAlign(0,0);
-  g.setFont("Vector",50);
-  g.drawString(time, 50, 50, true);
-}
-
-// Clear the screen once, at startup
 g.clear();
-// draw immediately at first
-draw();
+// work out how to display the current time
+
+var time = "teste display";
+
+// Reset the state of the graphics library
+g.reset();
+// draw the current time
+g.setFontAlign(0,0);
+g.setFont("Vector",50);
+g.drawString(time, 50, 50, true);
+
