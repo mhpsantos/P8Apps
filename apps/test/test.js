624bb677ce4dd0a91f04eede6fa0840c427fa637
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 10:54:19 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,4 +1,19 @@
-g.reset();
-require("FontTeletext10x18Mode7").add(Graphics);
-g.setFontTeletext10x18Mode7();
-g.drawString("0123456789[]");
+function draw() {
+  // work out how to display the current time
+  var d = new Date();
+  var h = d.getHours(), m = d.getMinutes();
+  var time = h + ":" + ("0"+m).substr(-2);
+
+  // Reset the state of the graphics library
+  g.reset();
+  // Clear the area where we want to draw the time
+  g.clearRect(50,50,100,120);
+  // draw the current time
+  g.drawString(time, 50, 50);
+}
+
+// Clear the screen once, at startup
+g.clear();
+// draw immediately at first
+draw();
+var secondInterval = setInterval(draw, 1000);
