03c7338725612c7f5a31b0c002f3bbb93e4e193d
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 12:38:38 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,4 +1,13 @@
-g.clear(true);
-// work out how to display the current time
-g.setFont("Vector", 50);
-g.drawString("TESTE");
+var counter = 30;
+
+function countDown() {
+  counter--;
+
+  g.clear();
+  // draw the current counter value
+  g.drawString(counter, g.getWidth()/2, g.getHeight()/2);
+  // optional - this keeps the watch LCD lit up
+  g.flip();
+}
+
+var interval = setInterval(countDown, 1000);
