db951645e8cf73db07be9f6abfc2f39fde5ab60f
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 11:12:55 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -2,12 +2,13 @@ function draw() {
   // Reset the state of the graphics library
   g.reset();
   // Clear the area where we want to draw the time
-   g.clearRect(50,50,100,120);
+  g.clearRect(50,50,100,120);
   // draw the current time
-  g.drawString("12345678890", 50, 50, true);
+  g.drawString("test", 50, 50);
 }
 
 // Clear the screen once, at startup
 g.clear();
 // draw immediately at first
 draw();
+var secondInterval = setInterval(draw, 1000);
