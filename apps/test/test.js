1b7c89d23dabdefbb2ccc1f40766f120de338e41
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 14:12:20 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -6,10 +6,15 @@ function drawtext() {
   
   g.clear();
   // draw the current counter value
-  g.drawString(variable, g.getWidth()/2, g.getHeight()/2);
+  g.setFont("Vector", 50);
+  g.setFontAlign(0,0);
+  g.drawString(variable);
   // optional - this keeps the watch LCD lit up
   g.flip();
 }
 
 g.clear();
+
 drawtext();
+
+var interval = setInterval(drawtext, 1000);
