6c1c8f7d489c8566d0179349d995375c5eb58550
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-05 12:09:23 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,11 +1,4 @@
 g.clear();
-// work out how to display the current time
-
-var time = "teste display";
-
-// Reset the state of the graphics library
 g.reset();
-// draw the current time
-g.setFontAlign(0,0);
-g.setFont("Vector",50);
-g.drawString(time);
+// work out how to display the current time
+Bangle.Buzz(100, 1);
