fd069da026495016119523792389e547bcc35590
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-06 17:33:11 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -4,12 +4,17 @@ var verts = [-5,-5, 5,-5, 5,5, -5,5];
 
 function draw(){
   g.clear();
-  var verts2 = g.transformVertices(verts, {x:g.getWidth()/2, y:g.getHeight()/2,scale:8, rotate:i});
+  if(i%2==0){
+    scale = 0.5;
+  }else{
+    scale=0.75;
+  }
+  var verts2 = g.transformVertices(verts, {x:g.getWidth()/2, y:g.getHeight()/2,scale:3+scale, rotate:0});
 
   g.fillPoly(verts2);
   
-  i+=0.5;
+  i++;
   
 }
 
-var interval = setInterval(draw, 50);
+var interval = setInterval(draw, 500);
