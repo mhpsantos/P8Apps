62e4459548b7920adbbdec393e94366ce54a9881
Matheus Santos <52681958+mhpsantos@users.noreply.github.com>
2021-07-07 10:21:56 -0300

    Update test.js

--- apps/test/test.js
+++ apps/test/test.js
@@ -1,1 +1,144 @@
-var buf=Graphics.createArrayBuffer(240,240,1,{msb:!0}),img=require("heatshrink").decompress(atob("sFg4MA/4ACI/4AYgwLKh4LKj4LKn4LKv4LKSpUBBZUDBZUHBZUP//4LxLSB4BeIBYPwLxALB/wLKHpALDGA4vC//gI5JVIL4JtB/h3JJBCPCEwKnJEwOAX5AaBBY7XBDQJgIDQRgIDQQLHgwaCVJAaCPAkPPYoLEj5NEBYs/JokHSAgLFgYLEv4LFTggLMHYkBBaChFBYy3HBf4LuR4wLLWaLXWfZc/8ALJj/wHYn4BZMAAokfCIgAFh4pCsALGg5NCEwoLCRIQmHgf/QgJQEUIhsBOggLEFoLDGYoQtBEwQAFv4VBEwQAFn//8ABBBY0f/4ABMBIABMBIABMA45BAAJgHJAQLJh4jKGATDJKoJTHHoZrHBYaDHBYabHBYYKJZZAADLxAACLxIABLxUAsALKAH4ANA==")),i=0,verts=[-5,-5,5,-5,5,5,-5,5];function flip(){g.drawImage({width:240,height:240,bpp:1,buffer:buf.buffer},0,0)}var interval=setInterval(function(){buf.clear(),buf.drawImage(img,g.getWidth()/2,g.getHeight()/2,{scale:1,rotate:i}),flip(),i-=.5},100);
+require("Font8x16").add(Graphics);
+
+var numerals = {
+    0: [
+        [9, 1, 82, 1, 90, 9, 90, 92, 82, 100, 9, 100, 1, 92, 1, 9, 9, 1],
+        [30, 25, 61, 25, 69, 33, 69, 67, 61, 75, 30, 75, 22, 67, 22, 33, 30, 25]
+    ],
+    1: [
+        [50, 1, 82, 1, 90, 9, 90, 92, 82, 100, 73, 100, 65, 92, 65, 27, 50, 27, 42, 19, 42, 9, 50, 1]
+    ],
+    2: [
+        [9, 1, 82, 1, 90, 9, 90, 53, 82, 61, 21, 61, 21, 74, 82, 74, 90, 82, 90, 92, 82, 100, 9, 100, 1, 92, 1, 48, 9, 40, 70, 40, 70, 27, 9, 27, 1, 19, 1, 9, 9, 1]
+    ],
+    3: [
+        [9, 1, 82, 1, 90, 9, 90, 92, 82, 100, 9, 100, 1, 92, 1, 82, 9, 74, 70, 74, 70, 61, 9, 61, 1, 53, 1, 48, 9, 40, 70, 40, 70, 27, 9, 27, 1, 19, 1, 9, 9, 1]
+    ],
+    4: [
+        [9, 1, 14, 1, 22, 9, 22, 36, 69, 36, 69, 9, 77, 1, 82, 1, 90, 9, 90, 92, 82, 100, 78, 100,
+            70, 92, 70, 61, 9, 61, 1, 53, 1, 9, 9, 1
+        ]
+    ],
+    5: [
+        [9, 1, 82, 1, 90, 9, 90, 19, 82, 27, 21, 27, 21, 40, 82, 40, 90, 48, 90, 92, 82, 100, 9, 100, 1, 92, 1, 82, 9, 74, 71, 74, 71, 61, 9, 61, 1, 53, 1, 9, 9, 1]
+    ],
+    6: [
+        [9, 1, 82, 1, 90, 9, 90, 19, 82, 27, 22, 27, 22, 40, 82, 40, 90, 48, 90, 92, 82, 100, 9, 100, 1, 92, 1, 9, 9, 1],
+        [22, 60, 69, 60, 69, 74, 22, 74, 22, 60]
+    ],
+    7: [
+        [9, 1, 82, 1, 90, 9, 90, 15, 20, 98, 9, 98, 1, 90, 1, 86, 56, 22, 9, 22, 1, 14, 1, 9, 9, 1]
+    ],
+    8: [
+        [9, 1, 82, 1, 90, 9, 90, 92, 82, 100, 9, 100, 1, 92, 1, 9, 9, 1],
+        [22, 27, 69, 27, 69, 43, 22, 43, 22, 27],
+        [22, 58, 69, 58, 69, 74, 22, 74, 22, 58]
+    ],
+    9: [
+        [9, 1, 82, 1, 90, 9, 90, 92, 82, 100, 9, 100, 1, 92,
+            1, 82, 9, 74, 69, 74, 69, 61, 9, 61, 1, 53, 1, 9, 9, 1
+        ],
+        [22, 27, 69, 27, 69, 41, 22, 41, 22, 27]
+    ]
+};
+
+function transformN(a, b, c, d) {
+    return g.transformVertices(numerals[a][b], {
+        x: c,
+        y: d,
+        scale: 1.1
+    })
+}
+
+function drawH1(a) {
+    layer1 = transformN(a, 0, 30, 5);
+    layer2 = transformN(a, 1, 30, 5);
+    layer3 = transformN(a, 2, 30, 5);
+    g.setColor(0xFFff5555);
+    g.fillPoly(layer1);
+    g.setColor(0, 0, 0);
+    g.fillPoly(layer2);
+    g.fillPoly(layer3)
+}
+
+function drawH2(a) {
+    layer1 = transformN(a, 0, 140, 5);
+    layer2 = transformN(a, 1, 140, 5);
+    layer3 = transformN(a, 2, 140, 5);
+    g.setColor(16733525);
+    g.fillPoly(layer1);
+    g.setColor(0, 0, 0);
+    g.fillPoly(layer2);
+    g.fillPoly(layer3)
+}
+
+function drawM1(a) {
+    layer1 = transformN(a, 0, 30, 125);
+    layer2 = transformN(a, 1, 30, 125);
+    layer3 = transformN(a, 2, 30, 125);
+    g.setColor(16777215);
+    g.fillPoly(layer1);
+    g.setColor(0, 0, 0);
+    g.fillPoly(layer2);
+    g.fillPoly(layer3)
+}
+
+function drawM2(a) {
+    layer1 = transformN(a, 0, 140, 125);
+    layer2 = transformN(a, 1, 140, 125);
+    layer3 = transformN(a, 2, 140, 125);
+    g.setColor(16777215);
+    g.fillPoly(layer1);
+    g.setColor(0, 0, 0);
+    g.fillPoly(layer2);
+    g.fillPoly(layer3);
+  
+}
+function drawDate(){
+  g.setFont8x16();
+g.drawString("0123456789");
+}
+function drawTime() {
+  g.setRotation(0);
+  g.reset();
+    g.clear();
+    var a = "0" + (new Date).getHours(),
+        b = (new Date).getMinutes(),
+        c = (new Date).getDate(),
+        d = (new Date).getMonth();
+    a = 10 > a ? ("0" + (new Date).getHours()).substr(-2) : ("0" + (new Date).getHours()).substr(1, 2);
+    b = 10 > b ? ("0" + (new Date).getMinutes()).substr(-2) : ("0" + (new Date).getMinutes()).substr(1, 2);
+    c = 10 > a ? ("0" + (new Date).getDate()).substr(-2) : ("0" + (new Date).getDate()).substr(1, 2);   
+    d = 10 < a ? ("0" + (new Date).getMonth()).substr(-2) : ("0" + ((new Date).getMonth()+1)).substr(1, 2);
+    drawH1(a[0]);
+    drawH2(a[1]);
+    drawM1(b[0]);
+    drawM2(b[1]);
+    drawDate(c,d);
+}
+
+
+function drawDate(date, month){
+  g.setColor(-1);
+  g.setRotation(3);
+  g.setFont8x16();
+  g.drawString(date + "-" + month,104);
+}
+function stopDraw() {
+    interval && clearInterval(interval);
+    g.clear()
+}
+
+function refresh() {
+    0 === (new Date).getSeconds() && drawTime();
+}
+
+P8.on("sleep", function(a) {
+    a ? stopDraw() : (drawTime(), refresh())
+});
+
+setTimeout(() => {
+    drawTime();
+}, 100);
+var interval = setInterval(refresh, 1E3);
+
